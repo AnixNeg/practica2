4e2a49ea276b468cbd8d13d35cf71e35f9d72286
AnixNeg <anix_05katy@hotmail.com>
2014-03-07 13:01:56 -0600

    n_n

---
+++ practica2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace proyecto2
+{
+	class MainClass
+	{
+
+		public static void Main (string[] args)
+		{
+			double iva = 1.16;
+			double precio1,precio2,precio3;
+
+			Console.WriteLine ("Ingresa el primer valor");
+			float primer=float.Parse (Console.ReadLine ());
+			precio1 = primer * iva;
+			Console.WriteLine ("El precio normal es:"+ primer);
+			Console.WriteLine ("El Precio con iva es:"+ precio1);
+
+			Console.WriteLine ("Ingresa el segundo valor");
+			float segundo=float.Parse(Console.ReadLine ());
+			precio2 = segundo * iva;
+			Console.WriteLine ("El precio normal es:"+ segundo);
+		    Console.WriteLine ("El Precio con iva es:" + precio2);
+
+
+			Console.WriteLine ("Ingresa el tercer valor");
+			float tercero=float.Parse(Console.ReadLine ());
+			precio3 = tercero * iva;
+			Console.WriteLine ("El precio normal es:"+ tercero);
+			Console.WriteLine ("El Precio con iva es:" + precio3);
+
+			Console.WriteLine ("Preciona cualquier tecla para salir");
+	
+		}
+	}
+}
